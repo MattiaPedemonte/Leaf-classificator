9110d87c370bf2b95b177a2a20f2d8989fc51fc1
Mattia Pedemonte <mattiapedemonte98@gmail.com>
2024-10-31 15:56:36 +0100

    minor

--- ClassificationModel.py
+++ ClassificationModel.py
@@ -1,12 +1,14 @@
 # CNN for classification on Grapevine leaves
 from importLibrary import *
 
+INPUT_SHAPE=(511, 511, 3)
+
 def GenerateVGG19Model(num_classes: int) -> Model:
     # Load the VGG19 model without the top layer (i.e., the fully connected layers)
     vgg19_model = tf.keras.applications.VGG19(
         include_top=False,        # Exclude the top layer
         weights='imagenet',      # Load pre-trained weights from ImageNet
-        input_shape=(256, 256, 3) # Specify input shape
+        input_shape=INPUT_SHAPE # Specify input shape
     )
     
     for layer in vgg19_model.layers[:-4]:  # Freeze all layers except the last 4
@@ -30,7 +32,7 @@ def GenerateVGG16Model(num_classes: int) -> Model:
     vgg16_model = tf.keras.applications.VGG16(
         include_top=False,        # Exclude the top layer
         weights='imagenet',      # Load pre-trained weights from ImageNet
-        input_shape=(256, 256, 3) # Specify input shape
+        input_shape=INPUT_SHAPE # Specify input shape
     )
     
     # for layer in vgg16_model.layers[:-4]:  # Freeze all layers except the last 4
@@ -40,9 +42,11 @@ def GenerateVGG16Model(num_classes: int) -> Model:
     x = vgg16_model.output
     x = GlobalAveragePooling2D()(x)
     # x = Flatten()(x)
-    x = Dropout(0.5)(x)  # Dropout per regolarizzazione
+    x = Dropout(0.2)(x)  # Dropout per regolarizzazione
+    x = Dense(256, activation='relu')(x)
+    x = Dropout(0.2)(x)  # Dropout per regolarizzazione
     x = Dense(64, activation='relu')(x)
-    x = Dropout(0.25)(x)  # Dropout per regolarizzazione
+    x = Dropout(0.2)(x)  # Dropout per regolarizzazione
     predictions = Dense(num_classes, activation='softmax')(x)
     model = Model(inputs=vgg16_model.input, outputs=predictions)
 
@@ -56,7 +60,7 @@ def GenerateResModel(num_classes: int) -> Model:
     res_model = resnet50.ResNet50(
         include_top=False,        # Exclude the top layer
         weights='imagenet',      # Load pre-trained weights from ImageNet
-        input_shape=(256, 256, 3) # Specify input shape
+        input_shape=INPUT_SHAPE # Specify input shape
     )
     
     ## freeze some layer 
@@ -82,7 +86,7 @@ def GenerateResModel(num_classes: int) -> Model:
 
 def GenerateCNN(num_classes: int) -> Model:
     # Definizione dell'input
-    input_layer = tf.keras.Input(shape=(256, 256, 3))
+    input_layer = tf.keras.Input(shape=INPUT_SHAPE)
 
     # Primo livello convoluzionale
     x = layers.Conv2D(filters=32,               # Numero di filtri
