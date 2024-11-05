163902433f75988dda0515422b43fc864ec3e63b
Mattia Pedemonte <mattiapedemonte98@gmail.com>
2024-11-05 16:23:24 +0100

    93% accuracy

--- ClassificationModel.py
+++ ClassificationModel.py
@@ -3,6 +3,58 @@ from importLibrary import *
 
 INPUT_SHAPE=(256, 256, 3)
 
+def OutputNetwork(num_classes: int, input_layer: layers) -> layers:
+    """
+    Crea una rete neurale che può essere utilizzata come testa finale per un modello,
+    aggiungendo strati densi e uno strato di classificazione softmax.
+
+    :param num_classes: Numero di classi per la classificazione, corrispondente ai nodi di output finali.
+    :param x: Input layer o output di un modello pre-addestrato su cui aggiungere questa testa finale.
+    :return: Un modello Keras che rappresenta la testa finale, pronto per essere unito ad altri modelli.
+    """
+
+    # Layer possibili:
+    # x = GlobalAveragePooling2D()(x)
+
+    # Flatten: appiattisce l'input (es. una mappa di attivazione 2D) in un vettore 1D.
+    x = Flatten()(input_layer)
+    # Primo strato Dropout per la regolarizzazione, con tasso di abbandono del 50%.
+    x = Dropout(0.5)(x)
+    
+    # Strato Dense di 256 neuroni
+    x = Dense(256)(x)
+    # BatchNormalization per normalizzare l'output del livello Dense, migliorando la stabilità dell'allenamento.
+    x = BatchNormalization()(x)
+    #Activation relu applicata dopo BatchNorm per normalizzare sul lineare e non perdere informazioni
+    x = Activation('relu')(x)
+    # Secondo strato Dropout per ridurre l'overfitting, sempre con tasso di abbandono del 50%.
+    x = Dropout(0.5)(x)
+    
+    # Strato Dense di 120 neuroni con attivazione ReLU, che aggiunge ulteriore capacità di apprendimento.
+    x = Dense(120)(x)
+    # BatchNormalization per migliorare la velocità e stabilità dell'allenamento in questo livello.
+    x = BatchNormalization()(x)
+    #Activation relu applicata dopo BatchNorm per normalizzare sul lineare e non perdere informazioni
+    x = Activation('relu')(x)
+    # Terzo strato Dropout per regolarizzazione, con tasso di abbandono del 35%.
+    x = Dropout(0.35)(x)
+    
+    # Strato Dense di 60 neuroni con attivazione ReLU, ultimo strato denso prima dell'output.
+    x = Dense(60)(x)
+    # BatchNormalization per normalizzare l'output dell'ultimo livello Dense.
+    x = BatchNormalization()(x)
+    #Activation relu applicata dopo BatchNorm per normalizzare sul lineare e non perdere informazioni
+    x = Activation('relu')(x)
+
+    # Strato finale Dense per la classificazione: num_classes neuroni con attivazione softmax,
+    # per ottenere una distribuzione di probabilità sulle classi.
+    predictions = Dense(num_classes, activation='softmax')(x)
+    
+    # Creazione del modello, che prende come input 'x' e restituisce 'predictions'.
+    #model = Model(inputs=x, outputs=predictions, name="OutputNetwork")
+    
+    return predictions
+
 def GenerateVGG19Model(num_classes: int) -> Model:
     # Load the VGG19 model without the top layer (i.e., the fully connected layers)
     vgg19_model = tf.keras.applications.VGG19(
@@ -17,24 +69,9 @@ def GenerateVGG19Model(num_classes: int) -> Model:
 
     x = vgg19_model.output
 
-    x = Flatten()(x)
-    x = Dropout(0.5)(x)  # Dropout per regolarizzazione
+    output_layer = OutputNetwork(num_classes=num_classes, input_layer=x)    
 
-    x = Dense(256, activation='relu')(x)
-    x = BatchNormalization()(x)
-    x = Dropout(0.5)(x)  # Dropout per regolarizzazione
-
-    x = Dense(120, activation='relu')(x)
-    x = BatchNormalization()(x)
-    x = Dropout(0.35)(x)  # Dropout per regolarizzazione
-
-    x = Dense(60, activation='relu')(x)
-    x = BatchNormalization()(x)
-    # x = Dropout(0.2)(x)  # Dropout per regolarizzazione
-    predictions = Dense(num_classes, activation='softmax')(x)
-    
-
-    model = Model(inputs=vgg19_model.input, outputs=predictions)
+    model = Model(inputs=vgg19_model.input, outputs=output_layer)
 
     # Check the model summary
     model.summary()
@@ -54,24 +91,9 @@ def GenerateVGG16Model(num_classes: int) -> Model:
     # vgg16_model.trainable = False
 
     x = vgg16_model.output
-    # x = GlobalAveragePooling2D()(x)
-    x = Flatten()(x)
-    x = Dropout(0.5)(x)  # Dropout per regolarizzazione
-    
-    x = Dense(256, activation='relu')(x)
-    x = BatchNormalization()(x)
-    x = Dropout(0.5)(x)  # Dropout per regolarizzazione
-    
-    x = Dense(120, activation='relu')(x)
-    x = BatchNormalization()(x)
-    x = Dropout(0.5)(x)  # Dropout per regolarizzazione
-    
-    x = Dense(60, activation='relu')(x)
-    x = BatchNormalization()(x)
-    x = Dropout(0.5)(x)  # Dropout per regolarizzazione
-    
-    predictions = Dense(num_classes, activation='softmax')(x)
-    model = Model(inputs=vgg16_model.input, outputs=predictions)
+    output_layer = OutputNetwork(num_classes=num_classes, input_layer=x)    
+
+    model = Model(inputs=vgg16_model.input, outputs=output_layer)
 
     # Check the model summary
     model.summary()
@@ -95,23 +117,9 @@ def GenerateResModel(num_classes: int) -> Model:
 
     x = res_model.output
     
-    x = Flatten()(x)
-    x = Dropout(0.3)(x)  # Dropout per regolarizzazione
+    output_layer = OutputNetwork(num_classes=num_classes, input_layer=x)    
 
-    x = Dense(256, activation='relu')(x)
-    x = BatchNormalization()(x)
-    x = Dropout(0.3)(x)  # Dropout per regolarizzazione
-
-    x = Dense(120, activation='relu')(x)
-    x = BatchNormalization()(x)
-    x = Dropout(0.3)(x)  # Dropout per regolarizzazione
-
-    x = Dense(60, activation='relu')(x)
-    x = BatchNormalization()(x)
-
-    predictions = Dense(num_classes, activation='softmax')(x)
-    
-    model = Model(inputs=res_model.input, outputs=predictions)
+    model = Model(inputs=res_model.input, outputs=output_layer)
 
     # Check the model summary
     model.summary()
@@ -176,3 +184,7 @@ def GenerateCNN(num_classes: int) -> Model:
     model.summary()
 
     return model
+
+if __name__ == '__main__':
+    num_classes=5
+    model = GenerateVGG19Model(num_classes)
